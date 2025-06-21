6c7f3a2c32f7bc6728f61b25a8d0cd0b5f3d2fd0
Arinjay Singh <arinjay.singh@gmail.com>
2025-06-20 20:23:37 -0400

    feat(gpt config): updated config to be gpt2-small rather than medium

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ from dataclasses import dataclass
 class GPTConfig:
     block_size = 1024  # context size
     vocab_size = 50257  # vocabulary size
-    n_layer = 24  # number of transformer blocks
-    n_head = 16  # number of attention heads
-    n_embd = 1024  # embedding dimension
+    n_layer = 12  # number of transformer blocks
+    n_head = 12  # number of attention heads
+    n_embd = 768  # embedding dimension
