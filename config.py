abdf4792119f7fa1cb5b48379db0e78e58f7e235
Arinjay Singh <arinjay.singh@gmail.com>
2025-06-20 21:58:54 -0400

    fix(config): added typing to attributes

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ from dataclasses import dataclass
 
 @dataclass
 class GPTConfig:
-    block_size = 1024  # context size
-    vocab_size = 50257  # vocabulary size
-    n_layer = 12  # number of transformer blocks
-    n_head = 12  # number of attention heads
-    n_embd = 768  # embedding dimension
+    block_size: int = 1024  # context size
+    vocab_size: int = 50257  # vocabulary size
+    n_layer: int = 12  # number of transformer blocks
+    n_head: int = 12  # number of attention heads
+    n_embd: int = 768  # embedding dimension
